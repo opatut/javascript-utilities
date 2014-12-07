aa47d6dcc694079648d41dcae06102d6820f8001
Maximilian Heinz <maximilian.heinz@sinnerschrader.com>
2014-12-07 15:23:34 +0100

    wrote tests for extract-dom-nodes.js

--- test/extract-dom-nodes.test.js
+++ test/extract-dom-nodes.test.js
@@ -10,8 +10,8 @@ describe('extractDomNodes()', function () {
         length: 0
     };
 
-    var $ = function () {
-        this.length = 4;
+    global.$ = function () {
+        this.length = 1;
         this.get = sinon.spy();
     };
 
@@ -21,7 +21,25 @@ describe('extractDomNodes()', function () {
 
     it('calls .get() if object is instanceof $', function () {
         var instance = new $();
+
         extractDomNodes(instance);
         assert(instance.get.calledOnce);
+    });
+
+    it('Returns the DOM object if just 1 node is found', function () {
+        var obj = {
+            length: undefined
+        };
+
+        assert.equal(extractDomNodes(obj), obj);
+    });
+
+    it('returns an array of nodes if more nodes are found', function () {
+        var obj = {
+            length: 4
+        };
+
+        expect(extractDomNodes(obj)).to.be.an('array');
+        assert.equal(extractDomNodes(obj).length, 4);
     });
 });
